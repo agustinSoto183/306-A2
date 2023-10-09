938d4d853cedec933e7710088b6e1a7b3ac211a0
Henissa Tong <hton347@UoA.auckland.ac.nz>
2023-10-09 17:13:04 +1300

    Cleaned up threshold code (no (max+min)/2 threshold)

--- thresholdForQuad/thresholdForQuad.ino
+++ thresholdForQuad/thresholdForQuad.ino
@@ -21,36 +21,22 @@ void loop(){
 }
 
 double threshold(int pinNum) {
-  double top = 0;
-  double bottom = 1024;
   double sum = 0; 
   int numReadings = 10000;
 
 
   for (int i = 1; i <= numReadings; i++) {
-    // The two if statements finds the maximum and minimum voltage values (0-1024) over the number of readings.
-    if (top < analogRead(pinNum)) {
-        top = analogRead(pinNum);
-      }
-    if (bottom > analogRead(pinNum)) {
-        bottom = analogRead(pinNum);
-      }
     // Cumulative sum of all the readings.
     sum += analogRead(pinNum);
   }
 
-  // Threshold value based on averaging the minimum and maximum values.
-  double threshold = (top + bottom) / 2;
-  //Serial.print("Threshold from averaging the min and max values is :");
-  //Serial.println(threshold);
-
   // Averaged cumulative sum of all the readings.
-  double threshold2 = sum / numReadings;
+  double threshold = sum / numReadings;
 
   Serial.print("Threshold from average is: ");
-  Serial.print(threshold2);
+  Serial.print(threshold);
   Serial.print(" for pin ");
   Serial.println(pinNum);
 
-  return threshold2; // threshold is the avg of max and min; threshold2 is the average of the 1000 readings
+  return threshold;
 }
